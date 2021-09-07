becaedef5092a25f7d10f12ce9422ecdefedf279
Youngking0914 <ace_lephant@naver.com>
2021-09-07 20:23:18 +0900

    CollectionView 만드는중

---
+++ NikeTheDrawNotiApp/NikeTheDrawNotiApp/Models/DrawInfo.swift
@@ -0,0 +1,17 @@
+//
+//  DrawInfo.swift
+//  NikeTheDrawNotiApp
+//
+//  Created by YoungK on 2021/09/07.
+//
+
+import Foundation
+
+class DrawInfo {
+    let name: String?
+    
+    init(_ params: Any) {
+        name = nil
+    }
+
+}
---
+++ NikeTheDrawNotiApp/NikeTheDrawNotiApp/Views/CardCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  CardCollectionViewCell.swift
+//  NikeTheDrawNotiApp
+//
+//  Created by YoungK on 2021/09/07.
+//
+
+import UIKit
+
+class CardCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
