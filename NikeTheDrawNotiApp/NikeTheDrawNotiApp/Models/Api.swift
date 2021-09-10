8bb2e37811dc75e26e8525dd7dca92785f147cb6
Youngking0914 <ace_lephant@naver.com>
2021-09-10 13:43:50 +0900

    라이브러리 설치
    SwiftSoup, Alamofire 설치

---
+++ NikeTheDrawNotiApp/NikeTheDrawNotiApp/Models/Api.swift
@@ -0,0 +1,12 @@
+//
+//  Api.swift
+//  NikeTheDrawNotiApp
+//
+//  Created by YoungK on 2021/09/10.
+//
+
+import Foundation
+
+func request(url: String?) -> URLRequest {
+    return URLRequest(url: url)
+}
