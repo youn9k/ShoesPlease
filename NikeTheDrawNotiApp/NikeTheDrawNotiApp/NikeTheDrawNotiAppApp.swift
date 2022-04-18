5db5d88e6eaf43b652f4e9062e7219cb0ce4b4f1
Youngking0914 <60254939+Youngking0914@users.noreply.github.com>
2022-04-18 22:41:13 +0900

    [Add] SwiftUI 프로젝트 생성
    기존 UIKit 프로젝트 파일 삭제 후 SwiftUI로 새로 생성

---
+++ NikeTheDrawNotiApp/NikeTheDrawNotiApp/NikeTheDrawNotiAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NikeTheDrawNotiAppApp.swift
+//  NikeTheDrawNotiApp
+//
+//  Created by YoungK on 2022/04/18.
+//
+
+import SwiftUI
+
+@main
+struct NikeTheDrawNotiAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
