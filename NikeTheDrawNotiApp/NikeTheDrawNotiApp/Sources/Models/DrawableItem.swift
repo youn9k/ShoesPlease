d25e88a887d416505f53db05685a8ab435ebbe04
Youngking0914 <60254939+Youngking0914@users.noreply.github.com>
2022-04-21 00:30:06 +0900

    [Add] DrawableItem 모델 추가

---
+++ NikeTheDrawNotiApp/NikeTheDrawNotiApp/Sources/Models/DrawableItem.swift
@@ -0,0 +1,15 @@
+//
+//  drawableItems.swift
+//  NikeTheDrawNotiApp
+//
+//  Created by YoungK on 2022/04/20.
+//
+
+import Foundation
+
+struct DrawableItem {
+    let title: String
+    let theme: String
+    let image: String
+    let href: String
+}
