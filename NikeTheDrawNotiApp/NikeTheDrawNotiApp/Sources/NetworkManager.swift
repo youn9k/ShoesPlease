bebf9f0c1abb4c6be144af5aeb049d3fed032f5e
Youngking0914 <60254939+Youngking0914@users.noreply.github.com>
2022-04-22 21:56:35 +0900

    [Add] NetworkManager 추가

---
+++ NikeTheDrawNotiApp/NikeTheDrawNotiApp/Sources/NetworkManager.swift
@@ -0,0 +1,22 @@
+//
+//  NetworkManager.swift
+//  NikeTheDrawNotiApp
+//
+//  Created by YoungK on 2022/04/22.
+//
+
+import Foundation
+import Alamofire
+
+class NetworkManager {
+    
+//    func request(url: String) {
+//        AF.request(url,
+//                   method: .get,
+//                   parameters: nil,
+//                   encoding: URLEncoding.default,
+//                   headers: Const.headers)
+//        .validate(statusCode: 200..<300)
+//        .publishString()
+//    }
+}
