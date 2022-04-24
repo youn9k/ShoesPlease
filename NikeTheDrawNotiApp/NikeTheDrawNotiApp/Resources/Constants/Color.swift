2fe65ced8111a0f958b14f5f20eb000216acc358
Youngking0914 <60254939+Youngking0914@users.noreply.github.com>
2022-04-24 17:40:50 +0900

    [Add] Color 에셋 추가
    cream 컬러 추가

---
+++ NikeTheDrawNotiApp/NikeTheDrawNotiApp/Resources/Constants/Color.swift
@@ -0,0 +1,13 @@
+//
+//  Color.swift
+//  NikeTheDrawNotiApp
+//
+//  Created by YoungK on 2022/04/24.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    static let cream = Color("cream")
+}
