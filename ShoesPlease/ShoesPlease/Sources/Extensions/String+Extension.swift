84712cb4710bcbee3edcb57bee5ec8182998503e
Youngking0914 <60254939+Youngking0914@users.noreply.github.com>
2022-08-08 18:28:21 +0900

    [CHORE] String 익스텐션 변경

--- ShoesPlease/ShoesPlease/Sources/Extensions/String+Extension.swift
+++ ShoesPlease/ShoesPlease/Sources/Extensions/String+Extension.swift
@@ -8,9 +8,30 @@
 import Foundation
 
 extension String {
-  func sliceFrom(start: String, to: String) -> String? {
-    guard let s = rangeOfString(start)?.endIndex else { return nil }
-    guard let e = rangeOfString(to, range: s..<endIndex)?.startIndex else { return nil }
-    return self[s..<e]
-  }
+    func index<S: StringProtocol>(of string: S, options: String.CompareOptions = []) -> Index? {
+        range(of: string, options: options)?.lowerBound
+    }
+    func endIndex<S: StringProtocol>(of string: S, options: String.CompareOptions = []) -> Index? {
+        range(of: string, options: options)?.upperBound
+    }
+    func indices<S: StringProtocol>(of string: S, options: String.CompareOptions = []) -> [Index] {
+        ranges(of: string, options: options).map(\.lowerBound)
+    }
+    func ranges<S: StringProtocol>(of string: S, options: String.CompareOptions = []) -> [Range<Index>] {
+        var result: [Range<Index>] = []
+        var startIndex = self.startIndex
+        while startIndex < endIndex,
+              let range = self[startIndex...]
+            .range(of: string, options: options) {
+            result.append(range)
+            startIndex = range.lowerBound < range.upperBound ? range.upperBound :
+            index(range.lowerBound, offsetBy: 1, limitedBy: endIndex) ?? endIndex
+        }
+        return result
+    }
+//    let str = "Hello, playground, playground, playground"
+//    str.index(of: "play")      // 7
+//    str.endIndex(of: "play")   // 11
+//    str.indices(of: "play")    // [7, 19, 31]
+//    str.ranges(of: "play")     // [{lowerBound 7, upperBound 11}, {lowerBound 19, upperBound 23}, {lowerBound 31, upperBound 35}]
 }
