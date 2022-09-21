35760e0ee9cf16feb2ec641dd9ec2a71d3c2ff74
Youngking0914 <60254939+Youngking0914@users.noreply.github.com>
2022-09-21 09:54:43 +0900

    [REFACTOR] dateToString 익스텐션 분리

---
+++ ShoesPlease/ShoesPlease/Sources/Extensions/Date+Extension.swift
@@ -0,0 +1,21 @@
+//
+//  Date+Extension.swift
+//  ShoesPlease
+//
+//  Created by YoungK on 2022/09/21.
+//
+
+import Foundation
+
+extension Date {
+    /// Date 를 String 으로 변환합니다.
+    /// - Parameter format: 원하는 변환 형식 ex) "M/dd"
+    /// - Returns: String: ex) "9/14"
+    func toString(format: String = "yyyy-MM-dd") -> String {
+        let formatter = DateFormatter()
+        formatter.dateFormat = format
+        let dateString = formatter.string(from: self)
+        print("dateToString:", dateString)
+        return dateString
+    }
+}
