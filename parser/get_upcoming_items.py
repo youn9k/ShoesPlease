bc9aff5d157ee1dff3e1977e5137f5096c5a1482
youn9k <ace_lephant@naver.com>
2023-04-02 13:04:42 +0900

    [CHORE] 테스트용 푸쉬

--- parser/get_upcoming_items.py
+++ parser/get_upcoming_items.py
@@ -6,7 +6,7 @@ import time
 
 
 def get_upcoming_items():
-    print("get_upcoming_items...")
+    print("get_upcoming_items... ")
     try:
         options = webdriver.ChromeOptions()
         user_agent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36"
