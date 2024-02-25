75fc17b5847f255026718532c54b46886c2ce929
youn9k <ace_lephant@naver.com>
2024-02-25 11:17:28 +0900

    [FIX] 날짜 표기 방식 변경에 따른 조치
    나이키에서 날짜 표기 방식을 4/5 에서 4.5. 과 같이  변경함

--- parser/get_release_date_for_item.py
+++ parser/get_release_date_for_item.py
@@ -10,12 +10,12 @@ def get_release_date_for_item(href):
     soup = BeautifulSoup(response.text, 'lxml')
     date = soup.find('div', class_='available-date-component').get_text()
 
-    print("date:", date) # "4/5 오전 1:00출시"
+    print("date:", date) # "4.5. 오전 1:00출시"
 
-    splited_string = date.split() # ['4/5', '오전', '1:00출시']
+    splited_string = date.split() # ['4.5.', '오전', '1:00출시']
 
-    month = int(splited_string[0].split("/")[0])
-    day = int(splited_string[0].split("/")[1])
+    month = int(splited_string[0].split(".")[0])
+    day = int(splited_string[0].split(".")[1])
 
     time_string = splited_string[2].replace("출시", "") # "1:00"
 
